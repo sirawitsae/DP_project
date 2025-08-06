41184b9730eac4030e8f491a04ce802c584ed3e3
sirawitsae <140615286+sirawitsae@users.noreply.github.com>
2025-08-06 09:29:40 +0100

    Delete DP_project directory

--- DP_project/code/Plotting/ATLAS_Style/atlasstyle-00-03-05/rootlogon.C
+++
@@ -1,9 +0,0 @@
-#include "AtlasStyle.C"
-#include "AtlasUtils.C"
-void rootlogon()
-{
-  // Load ATLAS style
-  gROOT->LoadMacro("AtlasStyle.C");
-	gROOT->LoadMacro("AtlasUtils.C");
-  SetAtlasStyle();
-}
